03d36e991d40dea26680220140ebdc79eb3c4b07
Robert <lober3464@gmail.com>
2022-09-20 00:57:19 -0300

    refactor: importing and using new components

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Form } from './components/form/index';
+import { Header } from './components/header';
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <Form />
+    </>
+  );
+};
+
+
